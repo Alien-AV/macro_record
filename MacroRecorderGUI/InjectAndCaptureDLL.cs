5164aa7d886b6670f4b3546abadf8f37dfea0c2c
Alien_AV <alienav@gmail.com>
2018-10-02 02:14:20 +0300

    set calling convention for dll imports
    still not resolving the x86 issue with calling the dll

--- MacroRecorderGUI/InjectAndCaptureDLL.cs
+++ MacroRecorderGUI/InjectAndCaptureDLL.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace MacroRecorderGUI
 {
@@ -6,19 +7,19 @@ namespace MacroRecorderGUI
     {
         public delegate void IacDllCaptureEventCb(IntPtr evtBufPtr, int bufSize);
 
-        [System.Runtime.InteropServices.DllImportAttribute("InjectAndCaptureDll.dll", EntryPoint = "iac_dll_init")]
+        [System.Runtime.InteropServices.DllImportAttribute("InjectAndCaptureDll.dll", EntryPoint = "iac_dll_init", CallingConvention = CallingConvention.Cdecl)]
         public static extern void iac_dll_init();
 
-        [System.Runtime.InteropServices.DllImportAttribute("InjectAndCaptureDll.dll", EntryPoint = "iac_dll_start_capture")]
+        [System.Runtime.InteropServices.DllImportAttribute("InjectAndCaptureDll.dll", EntryPoint = "iac_dll_start_capture", CallingConvention = CallingConvention.Cdecl)]
         public static extern void iac_dll_start_capture(IacDllCaptureEventCb cb);
 
-        [System.Runtime.InteropServices.DllImportAttribute("InjectAndCaptureDll.dll", EntryPoint = "iac_dll_stop_capture")]
+        [System.Runtime.InteropServices.DllImportAttribute("InjectAndCaptureDll.dll", EntryPoint = "iac_dll_stop_capture", CallingConvention = CallingConvention.Cdecl)]
         public static extern void iac_dll_stop_capture();
 
-        [System.Runtime.InteropServices.DllImportAttribute("InjectAndCaptureDll.dll", EntryPoint = "iac_dll_inject_event")]
+        [System.Runtime.InteropServices.DllImportAttribute("InjectAndCaptureDll.dll", EntryPoint = "iac_dll_inject_event", CallingConvention = CallingConvention.Cdecl)]
         public static extern void iac_dll_inject_event(IntPtr cppBuffer, int sizeOfCppBuffer);
 
-        [System.Runtime.InteropServices.DllImportAttribute("InjectAndCaptureDll.dll", EntryPoint = "iac_dll_inject_events")]
+        [System.Runtime.InteropServices.DllImportAttribute("InjectAndCaptureDll.dll", EntryPoint = "iac_dll_inject_events", CallingConvention = CallingConvention.Cdecl)]
         public static extern void iac_dll_inject_events(IntPtr cppBuffer, int sizeOfCppBuffer);
     }
 }
