eae8ee5ad8c936b97b05f51c9f82526553a73c69
Peretzl Levinov <perchick03@gmail.com>
2019-01-04 16:40:44 +0200

    Adding Capture Event Window

---
+++ MacroRecorderGUI/CaptureEventWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MacroRecorderGUI
+{
+    /// <summary>
+    /// Interaction logic for CaptureEventWindow.xaml
+    /// </summary>
+    public partial class CaptureEventWindow : Window
+    {
+        public CaptureEventWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
