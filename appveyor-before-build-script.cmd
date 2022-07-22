b0ad6d6dea448b87b9d986504cd8712636ff1aaa
Alien_AV <alienav@gmail.com>
2022-07-22 10:20:15 +0300

    regenerate protoc events locally on appveyor

--- appveyor-before-build-script.cmd
+++ appveyor-before-build-script.cmd
@@ -1,6 +1,4 @@
 nuget restore
-cd c:\tools\vcpkg
-git pull
-.\bootstrap-vcpkg.bat
 vcpkg install protobuf:%PLATFORM%-windows-static
 vcpkg integrate install
+.\Common\protobuf\regenerate-protoc-events.bat
