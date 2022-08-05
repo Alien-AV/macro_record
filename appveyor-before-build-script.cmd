414c785456d49c13e1ad839c66e39a8fcbf74b45
Alien-AV <alienav@gmail.com>
2022-08-05 23:01:01 +0300

    fix appveyor 32-bit build

--- appveyor-before-build-script.cmd
+++ appveyor-before-build-script.cmd
@@ -4,4 +4,4 @@ vcpkg integrate install
 cd Common\protobuf\
 rmdir /s /q cpp csharp
 mkdir cpp csharp
-C:\Tools\vcpkg\installed\x64-windows-static\tools\protobuf\protoc --cpp_out=cpp --csharp_out=csharp Events.proto
+C:\Tools\vcpkg\installed\%PLATFORM%-windows-static\tools\protobuf\protoc --cpp_out=cpp --csharp_out=csharp Events.proto
