5ffe3e99935c8bcee0fa29e226a8a324af257ea5
Alien_AV <alienav@gmail.com>
2022-08-05 16:36:38 +0300

    regenerate protoc files on appveyor
    upgrade innosetup to 6
    update auth_token for github releases deploy

--- appveyor-before-build-script.cmd
+++ appveyor-before-build-script.cmd
@@ -1,4 +1,7 @@
 nuget restore
 vcpkg install protobuf:%PLATFORM%-windows-static
 vcpkg integrate install
-.\Common\protobuf\regenerate-protoc-events.bat
+cd Common\protobuf\
+rmdir /s /q cpp csharp
+mkdir cpp csharp
+C:\Tools\vcpkg\installed\x64-windows-static\tools\protobuf\protoc --cpp_out=cpp --csharp_out=csharp Events.proto
