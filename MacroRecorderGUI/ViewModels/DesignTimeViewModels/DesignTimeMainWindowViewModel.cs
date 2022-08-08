71a74db4a02484066c95b2dc6c2574adf4706347
Alien_AV <alienav@gmail.com>
2022-08-08 22:10:11 +0300

    make XAML design view show dummy entries for easier design

---
+++ MacroRecorderGUI/ViewModels/DesignTimeViewModels/DesignTimeMainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using MacroRecorderGUI.Event;
+
+namespace MacroRecorderGUI.ViewModels.DesignTimeViewModels
+{
+    public class DesignTimeMainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<MacroViewModel> MacroTabs { get; set; }
+        private int _selectedTabIndex;
+        public int SelectedTabIndex
+        {
+            get => _selectedTabIndex;
+            set { _selectedTabIndex = value; OnPropertyChanged();}
+        }
+        public MacroViewModel ActiveMacro => (SelectedTabIndex != -1)? MacroTabs[SelectedTabIndex] : null;
+
+        public DesignTimeMainWindowViewModel()
+        {
+            MacroTabs = new ObservableCollection<MacroViewModel> { new MacroViewModel("DesignTimeDummyMacro"), new MacroViewModel("DesignTimeDummyMacro2") };
+            ActiveMacro.Events.Add(new KeyboardEvent(Key.A,false){TimeSinceStartOfRecording = 100});
+            ActiveMacro.Events.Add(new KeyboardEvent(Key.A,true){TimeSinceStartOfRecording = 200});
+            ActiveMacro.Events.Add(new MouseEvent(1,2,0x1){TimeSinceStartOfRecording = 300});
+        }
+    }
+}
